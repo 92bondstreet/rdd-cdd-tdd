8f98a436fabf140807074b2f891d0f9231e8453b
dfarah92 <farah.d92@hotmail.fr>
2016-02-23 16:25:34 +0100

    Update test-config.js

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,4 +1,4 @@
-var assert = require('assert'),
+/*var assert = require('assert'),
     config = require('./../lib/config');
 
 
@@ -9,3 +9,36 @@ describe('Config', function () {
 
     it('has correct key types')
 });
+*/
+
+	var expected = require('chai').expect;
+    var config = require('./../lib/config');
+
+    var units = {
+        type: 'si',
+        tmp: '˚C',
+        speed: 'mps'
+    };
+
+    var ip = '';
+
+    describe('Config', function () {
+
+        it('has 3 props', function () {
+            var Config = new config(units, ip);
+            expected(Config.args).to.exist;
+            expected(Config.ip).to.exist;
+            expected(Config.units).to.exist;
+        });
+
+        it('after construction, 2 props are populated', function () {
+            var Config = new config(units, ip);
+            expected(Config.args).to.not.equal(null);
+            expected(Config.units).to.not.equal(null);
+
+        });
+
+        it('has correct key types',function(){
+
+        });
+    });
